b1c08cd53dbe52d9551ef3fc3afadc3a705fd386
neural5torm <osasburg@gmail.com>
2020-05-19 21:11:54 +0200

    Add top nav bar + login/out

---
+++ DatingAppSPA/src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(response => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.error('Error while logging in.');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('User logged out.');
+  }
+}
